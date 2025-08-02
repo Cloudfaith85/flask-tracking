2df22d66d334baa773238d233ba5b5ebf56f042a
Cloudfaith85 <cloudfaith85@gmail.com>
2025-08-02 10:14:46 +0700

    main.py

--- main.py
+++ main.py
@@ -1,18 +1,11 @@
 from flask import Flask
 from tracking import check_and_notify
-import logging
 
 app = Flask(__name__)
 
-# Optional: Supaya log ditampilkan di console
-logging.basicConfig(level=logging.INFO)
-
 @app.route('/')
 def home():
-    logging.info("🔁 Checking tracking...")
     check_and_notify()
-    return "✅ Tracking checked and notified (if updated)"
+    return "✅ Tracking checked"
 
-if __name__ == '__main__':
-    # Jalankan di host='0.0.0.0' agar bisa diakses publik oleh UptimeRobot
-    app.run(host='0.0.0.0', port=3000)
+# Tidak perlu app.run() — karena akan dipanggil pakai gunicorn
